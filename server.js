c79da1f6fe37448395bcbb27adfefe7a08310a6a
joshie <joshlohrman@gmail.com>
2019-03-27 13:46:20 -0400

    add api data var

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send('api-data-fake'))
+app.get('/', (req, res) => res.send('API DATA '+process.env.API_DATA))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
